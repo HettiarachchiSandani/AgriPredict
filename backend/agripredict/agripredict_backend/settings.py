17d1d12d1498baf1dfee2161878350fcceafbc1d
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-02 07:08:52 +0530

    Add custom apps to INSTALLED_APPS in settings.py

--- backend/agripredict/agripredict_backend/settings.py
+++ backend/agripredict/agripredict_backend/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'core',
+    'owners',
+    'batches',
+    'feed',
+    'orders',
+    'predictions',
+    'reports',
 ]
 
 MIDDLEWARE = [
