b9be2f1f65f9b4e1690cc28a5708824805089100
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-07 12:29:03 +0530

    Registered core models in Django admin interface

--- backend/agripredict/core/admin.py
+++ backend/agripredict/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Role
 
-# Register your models here.
+# User admin
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('UserID', 'FirstName', 'LastName', 'RoleID')
+    search_fields = ('FirstName', 'LastName')
+    list_filter = ('RoleID',)
+
+# Role admin
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('RoleID', 'RoleName')
+    search_fields = ('RoleName',)
+
+# Register models with admin
+admin.site.register(User, UserAdmin)
+admin.site.register(Role, RoleAdmin)
