52899da843ce715f2c164be62f2141065b1ce47b
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-07 17:36:47 +0530

    Implemented viewsets for Breed, Batch, and DailyOperations

--- backend/agripredict/batches/views.py
+++ backend/agripredict/batches/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Batch, Breed, DailyOperations
+from .serializers import BatchSerializer, BreedSerializer, DailyOperationsSerializer
 
-# Create your views here.
+class BreedViewSet(viewsets.ModelViewSet):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+class BatchViewSet(viewsets.ModelViewSet):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
+
+class DailyOperationsViewSet(viewsets.ModelViewSet):
+    queryset = DailyOperations.objects.all()
+    serializer_class = DailyOperationsSerializer
