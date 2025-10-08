a9a2444b8298d4e6238fed45c30c8d10ac57a8d8
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-08 12:24:08 +0530

    Added viewsets

--- backend/agripredict/orders/views.py
+++ backend/agripredict/orders/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Buyer, Order, OrderBatch
+from .serializers import BuyerSerializer, OrderSerializer, OrderBatchSerializer
 
-# Create your views here.
+class BuyerViewSet(viewsets.ModelViewSet):
+    queryset = Buyer.objects.all()
+    serializer_class = BuyerSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderBatchViewSet(viewsets.ModelViewSet):
+    queryset = OrderBatch.objects.all()
+    serializer_class = OrderBatchSerializer
