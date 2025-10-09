8f0c1ea922240c89649e9fac40c53c1dce27e6af
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-09 12:21:37 +0530

    Implemented Viewsets

--- backend/agripredict/reports/views.py
+++ backend/agripredict/reports/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Reports, Records
+from .serializers import ReportsSerializer, RecordsSerializer
 
-# Create your views here.
+
+class ReportsViewSet(viewsets.ModelViewSet):
+    queryset = Reports.objects.all()
+    serializer_class = ReportsSerializer
+
+
+class RecordsViewSet(viewsets.ModelViewSet):
+    queryset = Records.objects.all()
+    serializer_class = RecordsSerializer
