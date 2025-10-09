f1360c766366c32a3b0db5c80de2a1c1bdafb18e
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-10-09 15:50:36 +0530

    Implemented viewsets

--- backend/agripredict/owners/views.py
+++ backend/agripredict/owners/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Owner, Manager, OwnerManagerBatch, OwnerManagerFeedstock
+from .serializers import (
+    OwnerSerializer, ManagerSerializer,
+    OwnerManagerBatchSerializer, OwnerManagerFeedstockSerializer
+)
 
-# Create your views here.
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class ManagerViewSet(viewsets.ModelViewSet):
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+
+class OwnerManagerBatchViewSet(viewsets.ModelViewSet):
+    queryset = OwnerManagerBatch.objects.all()
+    serializer_class = OwnerManagerBatchSerializer
+
+
+class OwnerManagerFeedstockViewSet(viewsets.ModelViewSet):
+    queryset = OwnerManagerFeedstock.objects.all()
+    serializer_class = OwnerManagerFeedstockSerializer
