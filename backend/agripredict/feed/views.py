e1daeaea8ae85c46a1c1d3cf636cacc78ff26d39
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2026-04-01 14:39:44 +0530

    Updated the views.py of feed

--- backend/agripredict/feed/views.py
+++ backend/agripredict/feed/views.py
@@ -1,7 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.db import transaction
 from .models import FeedStock
 from .serializers import FeedStockSerializer
+from rest_framework.permissions import IsAuthenticated
+from core.permission import IsOwnerOrManager
 
 class FeedStockViewSet(viewsets.ModelViewSet):
-    queryset = FeedStock.objects.all()
+    queryset = FeedStock.objects.all().order_by("stockid")
     serializer_class = FeedStockSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrManager]
+
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @transaction.atomic
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @transaction.atomic
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
