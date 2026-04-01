5b47615a129fe44f0e59ed0f496b0fe9c406efe2
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2026-04-01 14:43:21 +0530

    Change url paths of owners

--- backend/agripredict/owners/urls.py
+++ backend/agripredict/owners/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OwnerViewSet, ManagerViewSet, OwnerManagerBatchViewSet, OwnerManagerFeedstockViewSet
+from .views import (
+    OwnerViewSet,
+    ManagerViewSet,
+    OwnerManagerBatchViewSet,
+    OwnerManagerFeedstockViewSet,
+)
 
 router = DefaultRouter()
-router.register(r'owners', OwnerViewSet)
-router.register(r'managers', ManagerViewSet)
-router.register(r'owner-manager-batches', OwnerManagerBatchViewSet)
-router.register(r'owner-manager-feedstocks', OwnerManagerFeedstockViewSet)
+router.register('owners', OwnerViewSet)
+router.register('managers', ManagerViewSet)
+router.register('owner-manager-batches', OwnerManagerBatchViewSet)
+router.register('owner-manager-feedstocks', OwnerManagerFeedstockViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
