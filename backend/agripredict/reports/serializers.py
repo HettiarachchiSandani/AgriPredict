0ca35e23b7cc243248ab32cf4c9b5c0dd63cdf7a
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2026-04-01 14:47:14 +0530

    Updated the reports serializers.py

--- backend/agripredict/reports/serializers.py
+++ backend/agripredict/reports/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
-from .models import Record, Report
+from .models import Records, Report
 
 class ReportSerializer(serializers.ModelSerializer):
+    generatedby = serializers.StringRelatedField()
+    
     class Meta:
         model = Report
         fields = '__all__'
 
 class RecordSerializer(serializers.ModelSerializer):
+    batch_name = serializers.CharField(source='batchid.batchname', read_only=True)
+    operation_date = serializers.DateField(source='operationid.date', read_only=True)
+    entered_by_name = serializers.CharField(source='operationid.entered_by_id.username', read_only=True)
+
     class Meta:
-        model = Record
-        fields = '__all__'
+        model = Records
+        fields = [
+            "recordsid",
+            "operationid",
+            "batchid",
+            "timestamp",
+            "hashvalue",
+            "previoushash",
+            "batch_name",
+            "operation_date",
+            "entered_by_name",
+        ]
