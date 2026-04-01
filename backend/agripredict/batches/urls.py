0026f6d117efcaa588340a5c01c73548018cc090
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2026-04-01 14:34:25 +0530

    Change url paths of batches

--- backend/agripredict/batches/urls.py
+++ backend/agripredict/batches/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BatchViewSet, BreedViewSet, DailyOperationsViewSet
+from .views import BatchViewSet, BreedViewSet, DailyOperationsViewSet, DailySummaryViewSet
 
 router = DefaultRouter()
 router.register(r'breeds', BreedViewSet)
 router.register(r'batches', BatchViewSet)
 router.register(r'dailyoperations', DailyOperationsViewSet)
 
+daily_summary_view = DailySummaryViewSet.as_view({'get': 'summary'})
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('daily_summary/summary/', daily_summary_view, name='daily-summary'),
 ]
