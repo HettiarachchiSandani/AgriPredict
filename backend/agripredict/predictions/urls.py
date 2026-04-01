83fb4d9a7a4bc02bd894317d2063ec7b61d0e237
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2026-04-01 14:45:23 +0530

    Change url paths of predictions

--- backend/agripredict/predictions/urls.py
+++ backend/agripredict/predictions/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PredictionViewSet
 
 router = DefaultRouter()
-router.register('predictions', PredictionViewSet, basename='predictions')
+router.register('', PredictionViewSet, basename='predictions') 
 
 urlpatterns = [
     path('', include(router.urls)),
